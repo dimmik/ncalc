2153dee98126df83aae022808a912e11873006f8
dmk <git@dimmik.org>
2026-01-29 22:00:21 +0100

    build static

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ main.cpp -x c tinyexpr.c -o calculator.exe -lcomctl32 -mwindows -Os -s -ffunction-sections -fdata-sections -Wl,--gc-sections
+g++ -static main.cpp -x c tinyexpr.c -o calculator.exe -lcomctl32 -mwindows -Os -s -ffunction-sections -fdata-sections -Wl,--gc-sections
